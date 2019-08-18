2c1adcb54d1b3010448c38184dc563fe11e2ab6e
Hyeon Kim <simnalamburt@gmail.com>
2019-08-19 01:28:41 +0900

    Update gemini.sh

--- gemini.sh
+++ gemini.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -euo pipefail; IFS=$'\n\t'
 
+#
+# 안쓰는 패키지 삭제, 자주 쓰이는 패키지 기본으로 설치
+#
+yum autoremove -y postfix rpcbind
+yum update -y
+yum install -y tmux htop
+
+#
+# ssh 포트 2222로 변경. 22번 포트는 git.upnl.org이 사용해야한다.
+#
 KEY=Port
 VALUE=2222
 
@@ -12,5 +22,4 @@ else
   printf '%s %s\n' "$KEY" "$VALUE" >> /etc/ssh/sshd_config
 fi
 
-# sshd 재시작
 systemctl reload sshd
