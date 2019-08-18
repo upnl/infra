604fdb4b6a71a3b2f7a430a637035489df97e1ea
Hyeon Kim <simnalamburt@gmail.com>
2019-08-19 02:33:54 +0900

    cloudflare.tf: Import 4 A records

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,4 +1,37 @@
+locals {
+  domain = "upnl.org"
+}
+
 resource "cloudflare_zone" "upnl" {
-  zone = "upnl.org"
+  zone = local.domain
   plan = "free"
+}
+
+resource "cloudflare_record" "edhelbroy" {
+  domain = local.domain
+  name   = "edhelbroy.upnl.org"
+  type   = "A"
+  value  = "147.46.242.115"
+}
+
+resource "cloudflare_record" "sodrak" {
+  domain = local.domain
+  name   = "sodrak.upnl.org"
+  type   = "A"
+  value  = "147.46.242.158"
+}
+
+resource "cloudflare_record" "gemini" {
+  domain = local.domain
+  name   = "upnl.org"
+  type   = "A"
+  value  = "121.166.66.24"
+}
+
+# TODO: Remove
+resource "cloudflare_record" "uriel" {
+  domain = local.domain
+  name   = "uriel.upnl.org"
+  type   = "A"
+  value  = "147.46.113.114"
 }
