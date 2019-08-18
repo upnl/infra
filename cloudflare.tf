3f1f958af200bec02369b5a7dab756e28ac24be3
Hyeon Kim <simnalamburt@gmail.com>
2019-08-19 02:40:50 +0900

    cloudflare.tf: Import 5 CNAME records

--- cloudflare.tf
+++ cloudflare.tf
@@ -7,6 +7,9 @@ resource "cloudflare_zone" "upnl" {
   plan = "free"
 }
 
+#
+# 에델브로이
+#
 resource "cloudflare_record" "edhelbroy" {
   domain = local.domain
   name   = "edhelbroy.upnl.org"
@@ -14,24 +17,63 @@ resource "cloudflare_record" "edhelbroy" {
   value  = "147.46.242.115"
 }
 
+#
+# 소드락
+#
 resource "cloudflare_record" "sodrak" {
   domain = local.domain
   name   = "sodrak.upnl.org"
   type   = "A"
   value  = "147.46.242.158"
 }
+resource "cloudflare_record" "sodrak_wildcard" {
+  domain = local.domain
+  name   = "*.sodrak.upnl.org"
+  type   = "CNAME"
+  value  = "sodrak.upnl.org"
+}
+resource "cloudflare_record" "wiki" {
+  domain = local.domain
+  name   = "wiki.upnl.org"
+  type   = "CNAME"
+  value  = "sodrak.upnl.org"
+}
 
+#
+# 제미니
+#
 resource "cloudflare_record" "gemini" {
   domain = local.domain
   name   = "upnl.org"
   type   = "A"
   value  = "121.166.66.24"
 }
+resource "cloudflare_record" "gemini_sub" {
+  domain = local.domain
+  name   = "gemini.upnl.org"
+  type   = "CNAME"
+  value  = "upnl.org"
+}
+resource "cloudflare_record" "wildcard" {
+  domain = local.domain
+  name   = "*.upnl.org"
+  type   = "CNAME"
+  value  = "gemini.upnl.org"
+}
 
+#
+# 유리엘
 # TODO: Remove
+#
 resource "cloudflare_record" "uriel" {
   domain = local.domain
   name   = "uriel.upnl.org"
   type   = "A"
   value  = "147.46.113.114"
+}
+resource "cloudflare_record" "uriel_wildcard" {
+  domain = local.domain
+  name   = "*.uriel.upnl.org"
+  type   = "CNAME"
+  value  = "uriel.upnl.org"
 }
