0330db754e63df7195a2e0af21691655b115edde
Hyeon Kim <simnalamburt@gmail.com>
2019-08-23 21:43:10 +0900

    gemini.sh: jq, ripgrep 기본으로 설치하도록 설정

--- gemini.sh
+++ gemini.sh
@@ -5,37 +5,32 @@ set -euo pipefail; IFS=$'\n\t'
 set -x
 
 #
-# cloud-init이 호스트네임을 바꾸지 않도록 함
+# Hostname 설정
 #
 cat <<'EOF' > /etc/cloud/cloud.cfg.d/20_preserve_hostname.cfg
+# Hostname을 gemini로 고정하기 위함
 preserve_hostname: true
 EOF
-
-#
 # /etc/hosts에 매핑 추가
-#
 cat <<'EOF' >> /etc/hosts
 127.0.0.1 gemini
 EOF
-
-#
 # 호스트네임 설정
-#
 hostnamectl set-hostname gemini
 
 #
 # 안쓰는 패키지 삭제, 자주 쓰이는 패키지 기본으로 설치
 #
 yum autoremove -y postfix rpcbind
+yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
 yum update -y
-yum install -y tmux htop
+yum install -y tmux htop jq ripgrep
 
 #
 # ssh 포트 2222로 변경. 22번 포트는 git.upnl.org이 사용해야한다.
 #
 KEY=Port
 VALUE=2222
-
 if rg -q "^\s*$KEY" /etc/ssh/sshd_config; then
   # 키가 존재함, 찾아바꾸기 수행
   perl -pi -e "s|^\s*($KEY)\s+.*$|\1 $VALUE|g" /etc/ssh/sshd_config
@@ -43,7 +38,6 @@ else
   # 키가 존재하지 않음, 파일 하단에 append
   printf '%s %s\n' "$KEY" "$VALUE" >> /etc/ssh/sshd_config
 fi
-
 systemctl reload sshd
 
 #
