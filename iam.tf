8846cb2dc3633ac96408359d94dff3c8e24f63f8
Hyeon Kim <simnalamburt@gmail.com>
2019-08-26 21:49:30 +0900

    iam.tf: cloud-controller-manager 에 필요한 권한 부여

--- iam.tf
+++ iam.tf
@@ -90,6 +90,9 @@ resource "aws_iam_account_password_policy" "sane_default" {
   allow_users_to_change_password = true
 }
 
+#
+# 제미니용 Instance Profile
+#
 resource "aws_iam_role" "gemini" {
   name = "gemini"
   path = "/instance/"
@@ -108,8 +111,81 @@ resource "aws_iam_role" "gemini" {
 EOF
 }
 
-resource "aws_iam_policy" "access_volume" {
-  description = "Allow accessing EBS volumes"
+resource "aws_iam_policy" "kubernetes_master" {
+  description = "쿠버네티스 마스터에 필요한 권한들"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:DescribeTags",
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions",
+        "ec2:DescribeRouteTables",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeVolumes",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifyVolume",
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CreateRoute",
+        "ec2:DeleteRoute",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteVolume",
+        "ec2:DetachVolume",
+        "ec2:RevokeSecurityGroupIngress",
+        "ec2:DescribeVpcs",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:AttachLoadBalancerToSubnets",
+        "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancerPolicy",
+        "elasticloadbalancing:CreateLoadBalancerListeners",
+        "elasticloadbalancing:ConfigureHealthCheck",
+        "elasticloadbalancing:DeleteLoadBalancer",
+        "elasticloadbalancing:DeleteLoadBalancerListeners",
+        "elasticloadbalancing:DescribeLoadBalancers",
+        "elasticloadbalancing:DescribeLoadBalancerAttributes",
+        "elasticloadbalancing:DetachLoadBalancerFromSubnets",
+        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+        "elasticloadbalancing:ModifyLoadBalancerAttributes",
+        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+        "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:CreateListener",
+        "elasticloadbalancing:CreateTargetGroup",
+        "elasticloadbalancing:DeleteListener",
+        "elasticloadbalancing:DeleteTargetGroup",
+        "elasticloadbalancing:DescribeListeners",
+        "elasticloadbalancing:DescribeLoadBalancerPolicies",
+        "elasticloadbalancing:DescribeTargetGroups",
+        "elasticloadbalancing:DescribeTargetHealth",
+        "elasticloadbalancing:ModifyListener",
+        "elasticloadbalancing:ModifyTargetGroup",
+        "elasticloadbalancing:RegisterTargets",
+        "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
+        "iam:CreateServiceLinkedRole",
+        "kms:DescribeKey"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "kubernetes_node" {
+  description = "쿠버네티스 노드에 필요한 권한들"
   policy      = <<EOF
 {
   "Version": "2012-10-17",
@@ -119,33 +195,6 @@ resource "aws_iam_policy" "access_volume" {
       "Action": [
         "ec2:DescribeInstances",
         "ec2:DescribeRegions",
-        "ec2:DescribeRouteTables",
-        "ec2:DescribeSecurityGroups",
-        "ec2:DescribeSubnets",
-        "ec2:DescribeVolumes",
-        "ec2:DescribeVpcs",
-        "ec2:CreateVolume",
-        "ec2:ModifyVolume",
-        "ec2:AttachVolume",
-        "ec2:DeleteVolume",
-        "ec2:DetachVolume"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "access_ecr" {
-  description = "Allow accessing ECR"
-  policy      = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
         "ecr:GetAuthorizationToken",
         "ecr:BatchCheckLayerAvailability",
         "ecr:GetDownloadUrlForLayer",
@@ -161,14 +210,14 @@ resource "aws_iam_policy" "access_ecr" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "gemini_volume" {
+resource "aws_iam_role_policy_attachment" "kubernetes_master" {
   role       = aws_iam_role.gemini.name
-  policy_arn = aws_iam_policy.access_volume.arn
+  policy_arn = aws_iam_policy.kubernetes_master.arn
 }
 
-resource "aws_iam_role_policy_attachment" "gemini_ecr" {
+resource "aws_iam_role_policy_attachment" "kubernetes_node" {
   role       = aws_iam_role.gemini.name
-  policy_arn = aws_iam_policy.access_ecr.arn
+  policy_arn = aws_iam_policy.kubernetes_node.arn
 }
 
 resource "aws_iam_instance_profile" "gemini" {
