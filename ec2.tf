4c3af452a578193c16dabbddd4b4c3b40bd146f1
Hyeon Kim <simnalamburt@gmail.com>
2019-12-09 23:11:56 +0900

    ec2.tf: Allow IPv6 egress

--- ec2.tf
+++ ec2.tf
@@ -55,11 +55,12 @@ resource "aws_security_group_rule" "gemini_public_ports" {
 resource "aws_security_group_rule" "gemini_egress" {
   security_group_id = aws_security_group.gemini.id
 
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  type             = "egress"
+  from_port        = 0
+  to_port          = 0
+  protocol         = "-1"
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
 }
 
 resource "aws_key_pair" "sysadmin" {
