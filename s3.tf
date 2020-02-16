a271ff42e457b46aaff50d0166bc4c65f9bbd1f4
Hyeon Kim <simnalamburt@gmail.com>
2020-02-16 17:11:24 +0900

    s3.tf: Block public access towards 'upnl-backups'

--- s3.tf
+++ s3.tf
@@ -17,3 +17,12 @@ resource "aws_s3_bucket" "backups" {
   #  }
   #}
 }
+
+resource "aws_s3_bucket_public_access_block" "backups" {
+  bucket = aws_s3_bucket.backups.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
