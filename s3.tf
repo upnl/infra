953f0206ee1a03a84dbd5ee9e573d857b146f947
Hyeon Kim <simnalamburt@gmail.com>
2020-02-16 17:03:57 +0900

    s3.tf: New S3 bucket, "upnl-backups"

---
+++ s3.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "backups" {
+  bucket = "upnl-backups"
+
+  ## 자동 백업이 세팅되면 아래와 같은 방식으로 Lifecycle Rule을 만들어줍시다
+  #lifecycle_rule {
+  #  enabled = true
+  #  id      = "Transition UPnL Homepage backups to Glacier Deep Archive after 14 days"
+  #  prefix  = "upnl.org/"
+  #
+  #  # NOTE: STANDARD_IA 를 사용할 경우, S3 IA로 가는순간 30일치 요금이 무조건
+  #  # 계산된다는 점을 주의해주세요. Glacier로 보내게 될 경우, 90일이 지나야
+  #  # Glacier Deep Archive로 이동시킬 수 있습니다.
+  #
+  #  transition {
+  #    days          = 14
+  #    storage_class = "DEEP_ARCHIVE"
+  #  }
+  #}
+}
